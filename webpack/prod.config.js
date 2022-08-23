accaa912624dbc93a114038a2c362c1d5d9c8704
Markus Vuorio <markus.vuorio@iki.fi>
2022-08-23 15:51:51 +0300

    Fix production build (stupidity)

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -2,11 +2,12 @@ const webpack = require('webpack');
 const { merge } = require('webpack-merge');
 const { plugins } = require('./webpack.config.js');
 const baseConfig = require('./webpack.config.js');
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(baseConfig, {
     plugins: [
         new webpack.DefinePlugin({
-            "DB_URL": "https://archive.org/download/murolinks/murolinks.sqlite3"
+            "DB_URL": JSON.stringify("https://archive.org/download/murolinks/murolinks.sqlite3")
         })
     ]
 });
